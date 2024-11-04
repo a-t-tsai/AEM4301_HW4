f1180c7b16d59f1311214d9f9e53606186f988b2
Alan Tsai <167140522+a-t-tsai@users.noreply.github.com>
2024-11-03 20:09:21 -0600

    changed testing code

--- lambert.m
+++ lambert.m
@@ -1,20 +1,6 @@
-% Define initial and target positions, time intervals
-r0_1 = [0.5; 0.6; 0.7];
-r1_1 = [0.0; 1.0; 0.0];
-dt1 = 0.9667663;
-[v0_1, v1_1] = lambert_problem(r0_1, r1_1, dt1, -1);
-fprintf('v0long: [%.3g, %.4g, %.4g], v1long: [%.3g, %.3g, %.3g]\n', v0_1(1), v0_1(2), v0_1(3), v1_1(1), v1_1(2), v1_1(3));
-
-r0_2 = [1.0; 0.0; 0.0];
-r1_2 = [1.0; 0.125; 0.125];
-dt2 = 0.125;
-[v0_2, v1_2] = lambert_problem(r0_2, r1_2, dt2, 1);
-fprintf('v0short: [%.3g, %.4g, %.4g], v1short: [%.3g, %.3g, %.3g]\n', v0_2(1), v0_2(2), v0_2(3), v1_2(1), v1_2(2), v1_2(3));
-
-function [v0, v1] = lambert_problem(r0, r1, dt, direction)
+%************Orbital Mech. HW 4, Alan Tsai and Vivek Suthram***********
+function [v0, v1] = lambert(r0, r1, dt, direction)
     mu = 1.0; 
-    
-    
     r0_mag = norm(r0);
     r1_mag = norm(r1);
     theta = acos(dot(r0, r1) / (r0_mag * r1_mag));
@@ -53,7 +39,8 @@ function [v0, v1] = lambert_problem(r0, r1, dt, direction)
         y = r0_mag + r1_mag - A * (1 - z * S) / sqrt(C);
         X = sqrt(y / C);
         U = (1 / sqrt(mu)) * (X^3 * S + A * sqrt(y)) - dt;
-        V = (1 / sqrt(mu)) * (X^3 * (S_prime - (3 * S * C_prime) / (2 * C)) + (A / 8) * ((3 * S * sqrt(y)) / C + (A / X)));
+        V = (1 / sqrt(mu)) * (X^3 * (S_prime - (3 * S * C_prime) ...
+            / (2 * C)) + (A / 8) * ((3 * S * sqrt(y)) / C + (A / X)));
         
         % Update z and relative error
         z_new = z - U / V;
